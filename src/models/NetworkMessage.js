1ca3c671e00c696460158f4d5753a1970c75e372
Andreas Heimann <privat@andreas-heimann.com>
2020-12-27 18:10:57 +0100

    added server

--- src/models/NetworkMessage.js
+++
@@ -1,8 +0,0 @@
-class NetworkMessage {
-    constructor(_players) {
-        _players.forEach(function(o){ delete o.ws; });
-        this.players = _players;
-    }
-}
-
-module.exports = NetworkMessage;
